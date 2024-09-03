96d52d0b2eb201b1787890ce18c3085cb6883b31
Ankur Patel <32640688+ankur51206@users.noreply.github.com>
2024-09-03 22:14:04 +0530

    Create backend.tf

---
+++ environments/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "realimmerse_group"
+    storage_account_name = "realimmersegroupdiag"
+    container_name       = "tfstatestatic"
+    key                  = "dev.tfstate"
+  }
+}
