48aac683eeab142233b2210bcee85b8c13540b03
Ankur Patel <32640688+ankur51206@users.noreply.github.com>
2024-09-03 16:40:11 +0530

    Update main.tf

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault" "example" {
-  name                        = "${var.key_vault_name}-${var.environment_name}"
+  name                        = var.key_vault_name
   location                    = var.location
-  resource_group_name         = "${var.resource_group_name}-${var.environment_name}"
+  resource_group_name         = var.resource_group_name
   sku_name                    = "standard"
   tenant_id                   = var.tenant_id
 
