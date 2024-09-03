742e84b005755697626c110096bc5fee300c09d0
Ankur Patel <32640688+ankur51206@users.noreply.github.com>
2024-09-03 16:45:02 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source              = "../../modules/resource_group"
-  resource_group_name = var.resource_group_name
+  resource_group_name = "${var.resource_group_name}-${var.environment_name}"
   location            = var.location
 }
 
@@ -8,7 +8,7 @@ module "key_vault" {
   source              = "../../modules/key_vault"
   resource_group_name = var.resource_group_name
   location            = var.location
-  key_vault_name      = var.key_vault_name
+  key_vault_name      = "${var.key_vault_name}-${var.environment_name}"
   tenant_id           = var.tenant_id
   object_id           = var.object_id
   environment         = var.environment
