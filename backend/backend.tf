99be89a76b057fffc058c4879970753ea2dd923b
Ankur Patel <32640688+ankur51206@users.noreply.github.com>
2024-09-03 21:50:03 +0530

    Delete backend/backend.tf

--- backend/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "realimmerse_group"
-    storage_account_name = "realimmersegroupdiag"
-    container_name       = "tfstatestatic"
-    key                  = "terraform.tfstate"
-  }
-}
