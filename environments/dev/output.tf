a019061dfe14b4735631a19437836abd3b09b7ac
Ankur Patel <32640688+ankur51206@users.noreply.github.com>
2024-09-04 10:19:48 +0530

    Delete environments directory

--- environments/dev/output.tf
+++
@@ -1,9 +0,0 @@
-output "key_vault_id" {
-  description = "The ID of the Key Vault."
-  value       = module.key_vault.key_vault_id
-}
-
-output "key_vault_uri" {
-  description = "The URI of the Key Vault."
-  value       = module.key_vault.key_vault_uri
-}
